cc0f924fbecca116f457b9d38279de86775323b3
Fabio <lazzaroni@jeflab.it>
2022-06-22 17:08:36 +0200

    🥅: Add catch boundaries

--- app/routes/jokes/index.tsx
+++ app/routes/jokes/index.tsx
@@ -1,8 +1,7 @@
+import type {Joke} from "@prisma/client";
 import type {LoaderFunction} from "@remix-run/node";
 import {json} from "@remix-run/node";
-import {useLoaderData, Link} from "@remix-run/react";
-import type {Joke} from "@prisma/client";
-
+import {Link, useCatch, useLoaderData} from "@remix-run/react";
 import {db} from "~/utils/db.server";
 
 type LoaderData = {randomJoke: Joke};
@@ -14,6 +13,11 @@ export const loader: LoaderFunction = async () => {
     take: 1,
     skip: randomRowNumber,
   });
+  if (!randomJoke) {
+    throw new Response("No random joke found", {
+      status: 404,
+    });
+  }
   const data: LoaderData = {randomJoke};
   return json(data);
 };
@@ -28,6 +32,17 @@ export default function JokesIndexRoute() {
       <Link to={data.randomJoke.id}>"{data.randomJoke.name}" Permalink</Link>
     </div>
   );
+}
+
+export function CatchBoundary() {
+  const caught = useCatch();
+
+  if (caught.status === 404) {
+    return (
+      <div className="error-container">There are no jokes to display.</div>
+    );
+  }
+  throw new Error(`Unexpected caught response with status: ${caught.status}`);
 }
 
 export function ErrorBoundary() {
